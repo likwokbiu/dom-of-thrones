ed86db6bfe0249c3fa831168be2ebd6015637914
likwokbiu <li_bill@ymail.com>
2018-03-25 17:36:56 -0400

    Setup Setting

---
+++ js/scene2.js
@@ -0,0 +1,41 @@
+function scene2() {
+
+  console.log('Executing scene 2!');
+  stage = document.querySelector('#stage');
+
+  // Setup Stage
+  document.querySelector('#sets');
+
+  castle = document.querySelector('#castle');
+  stageImg = stage.querySelector('#stage-img');
+
+  castleImgSrc = castle.getAttribute('src');
+  stageImg.setAttribute('src', castleImgSrc);
+
+  // Setup Cast
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(function() {
+
+  })
+
+  // Stage direction 2
+
+  frame(function() {
+
+  })
+
+}
